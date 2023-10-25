6ea53229de0386ed8f71f2a6e41fda00b8817432
Itallo Vasconcelos <itallo.vasconcelos@versatushpc.com.br>
2023-10-24 22:36:07 -0300

    Adding cors and configure token

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import { resolve } from 'path';
 
 dotenv.config();
 
+import cors from 'cors';
+import helmet from 'helmet';
+
 import './database';
 import express from 'express';
 import home from './routes/home';
@@ -10,6 +13,21 @@ import user from './routes/user';
 import token from './routes/token';
 import aluno from './routes/aluno';
 import photo from './routes/photo';
+
+const whiteList = [
+  'http://335.199.119.222',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -19,6 +37,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
