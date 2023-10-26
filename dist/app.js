62d44735fd9e9630ca60e8e816d00ad5d99f1ae5
Itallo Vasconcelos <itallo.vasconcelos@versatushpc.com.br>
2023-10-26 13:07:05 -0300

    npm run build

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,5 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }/* eslint-disable import/no-extraneous-dependencies */
+var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
 var _path = require('path');
 
 _dotenv2.default.config();
